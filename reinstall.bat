a1f55f6836ae44dca1bab3ab7776dfc9d826f76c
Neolin <lnonly@163.com>
2025-03-17 11:31:11 +0800

    MNT: enhance reinstall script to clean up build directories

--- reinstall.bat
+++ reinstall.bat
@@ -16,3 +16,10 @@ pip show %PACKAGE_NAME% >nul 2>&1 && pip uninstall %PACKAGE_NAME% -y
 :: 自动匹配最新版本安装
 for /f "delims=" %%i in ('dir /b /od "%WHEEL_DIR%\%PACKAGE_NAME%-*.whl"') do set LATEST_WHL=%%i
 pip install "%WHEEL_DIR%\%LATEST_WHL%"
+
+echo Cleaning up build directories...
+if exist build rmdir /s /q build
+if exist lntools.egg-info rmdir /s /q lntools.egg-info
+if exist dist rmdir /s /q dist
+
+echo Build directories cleaned.
